31fb85e8a09e0c89fe7e7d73f04081f669bf321f
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-24 03:17:53 +0200

    Time: 43 ms (47.95%), Space: 19.4 MB (89.39%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+         vector< vector<string> > result;
+        map<string, int> m;
+        for(int i=0; i<strs.size(); i++){
+            string word = strs[i];
+            sort(word.begin(), word.end());
+            if (m.find(word)==m.end()){
+                vector<string> v;
+                v.push_back(strs[i]);
+                result.push_back(v);
+                m[word] = result.size()-1;
+                
+            }else{
+                result[m[word]].push_back(strs[i]);
+            }
+        }
+        
+        for(int i=0; i<result.size(); i++){
+            sort(result[i].begin(), result[i].end());
+        }
+        return result;
+    }
+    
+};
